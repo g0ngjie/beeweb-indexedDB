77957802ac54a13979d26fc2708331462659fdb9
Gj <gongjie0422@163.com>
2021-06-23 19:17:17 +0800

    fix: modify

--- test/index.js
+++ test/index.js
@@ -8,7 +8,8 @@ const db = new CommonDB("testDatabase", "person", {
 setTimeout(async () => {
   console.log("[debug]db.load:", db.load);
   if (db.load) {
-    // db.add({ name: "张三", mobile: "1312211" });
+    const create = await db.create({ _id: 1});
+    console.log("[debug]create:", create)
     // db.clear()
     // const findOne = await db.findOne(6);
     // console.log("[debug]findOne:", findOne);
@@ -16,13 +17,13 @@ setTimeout(async () => {
     // console.log("[debug]findAll:", findAll);
     // const find = await db.find("name", "张三");
     // console.log("[debug]find:", find);
-    // const update = await db.updateById(6, { aaa: 1 });
+    // const update = await db.updateById(1, { aaa: 1 });
     // console.log("[debug]update:", update);
     // const findLimit = await db.findLimit(2);
     // console.log("[debug]findLimit:", findLimit);
     // const findCount = await db.findCount();
     // console.log("[debug]findCount:", findCount);
-    const deleteById = await db.deleteById(7);
-    console.log("[debug]deleteById:", deleteById);
+    // const deleteById = await db.deleteById(1);
+    // console.log("[debug]deleteById:", deleteById);
   }
 }, 500);
